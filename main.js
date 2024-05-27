06e9f99561a31c12fac7501c0189bed09bed6084
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-27 20:41:18 +0300

    Traffic light-lab3

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function RedLight() {
+    document.getElementById('stopLight').style.backgroundColor = "red";
+    document.getElementById('slowLight').style.backgroundColor = "black";
+    document.getElementById('goLight').style.backgroundColor = "black";
+    }
+
+
+function YellowLight() {
+    document.getElementById('stopLight').style.backgroundColor = "black";
+    document.getElementById('slowLight').style.backgroundColor = "yellow";
+    document.getElementById('goLight').style.backgroundColor = "black";
+    }
+
+function GreenLight() {
+    document.getElementById('stopLight').style.backgroundColor = "black";
+    document.getElementById('slowLight').style.backgroundColor = "black";
+    document.getElementById('goLight').style.backgroundColor = "Green";
+    }
+
